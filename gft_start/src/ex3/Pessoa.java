8c4a96e087defa90fd10b17572eb13c5e6d6ff79
alepim <fatec.ale@gmail.com>
2022-04-01 23:07:23 -0300

    novo commit

--- gft_start/src/ex3/Pessoa.java
+++ gft_start/src/ex3/Pessoa.java
@@ -1,5 +1,64 @@
 package ex3;
 
-public class Pessoa {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Pessoa {
+	
+	private String nome;
+	private String nascimento;
+	private String sexo;
+	
+	public Pessoa(String nome, String nascimento, String sexo) {
+		this.nome = nome;
+		this.nascimento = nascimento;
+		this.sexo = sexo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	 static String anoAtual() {
+		DateFormat ano = new SimpleDateFormat("YYYY");
+		Date dat = new Date();
+		return ano.format(dat);
+	}
+	
+	static  Integer getAnoNascimento( Pessoa p) {
+		String [] s = p.getNascimento().split("/");
+		
+			return Integer.parseInt(s[2])	;
+	}
+	public static Integer getIdade( Pessoa p) {
+		return Integer.parseInt(anoAtual())- getAnoNascimento(p);
+	}
+	
+	public static void imprimeTempoVidaRestante(Pessoa p) {
+		if(p.getSexo()== "m" || p.getSexo()== "M" ) {
+			int tempo = 80- getIdade(p);
+			System.out.println(tempo);
+		}
+		if(p.getSexo()== "f" || p.getSexo()== "F" ) {
+			int tempo = 73- getIdade(p);
+			System.out.println("o tempo de vida restante é de "+ tempo +" anos!");
+		}
+	}
+	
+	
 }
--- gft_start/src/ex3/ex3main.java
+++ gft_start/src/ex3/ex3main.java
@@ -1,10 +1,17 @@
 package ex3;
 
+
+
 public class ex3main {
+	
 
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Pessoa p = new Pessoa("ana", "21/10/1990", "F");
+	Pessoa p1 = new Pessoa("kai", "21/10/1990", "M");
+    Pessoa.imprimeTempoVidaRestante(p);
+    Pessoa.imprimeTempoVidaRestante(p1);
 	}
 
 }
